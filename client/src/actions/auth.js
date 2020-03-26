874a678da1a16940af7ee154b450b0abb35df601
irwantosalt <irwanto.wibowo@salt.co.id>
2020-03-26 23:43:31 +0700

    membuat fitur logout dan konfiguasru nav link

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,8 @@ import {
   USER_LOADED,
   AUTH_ERR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from "./types";
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -92,4 +93,11 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+//Logout / Clear Profile
+export const logout = () => dispatch => {
+  dispatch({
+    type: LOGOUT
+  });
 };
