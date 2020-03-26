5b5d3114ef43d7abbdc4ad84880dee32ecc47edd
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-03-26 16:36:35 +0700

    alert component dan memanggil action (#30)
    Co-authored-by: irwantosalt <irwanto.wibowo@salt.co.id>

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,7 +2,12 @@ import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-const Register = () => {
+import { connect } from "react-redux";
+import { setAlert } from "../../actions/alert";
+
+import PropTypes from "prop-types";
+
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -19,7 +24,7 @@ const Register = () => {
     e.preventDefault();
 
     if (password !== password2) {
-      console.log("Password do not match");
+      setAlert("Password do not match", "danger");
     } else {
       // console.log(formData);
       const newUser = {
@@ -106,4 +111,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
