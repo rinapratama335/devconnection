8f247743a076573d93621436d7f6b497f10ef9fe
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-03-29 22:38:51 +0700

    38.membuat komponen profile (#38)
    * proteksi halaman dashboard (hanya untuk yang sudah login saja)
    
    * membuat komponent untuk form tambah data profile
    
    Co-authored-by: irwantosalt <irwanto.wibowo@salt.co.id>

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 
 //Redux
 import { Provider } from "react-redux";
@@ -39,6 +40,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
