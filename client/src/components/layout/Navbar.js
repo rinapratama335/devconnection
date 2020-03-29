12c6767ae59fc1b0211cc5a11980f302e869671f
irwantosalt <irwanto.wibowo@salt.co.id>
2020-03-29 16:37:44 +0700

    menampilkan data current user dan tampilan dasboard pertama

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,7 +9,13 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <a onClick={logout} href="#!">
+        <Link to="/dashboard">
+          <i className="fas fa-user"></i>{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href="/#!">
           <i className="fas fa-sign-out-alt"></i>{" "}
           <span className="hide-sm">Logout</span>
         </a>
