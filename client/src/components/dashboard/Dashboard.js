8eec6c87f68577536266538591fd8777a47880bb
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-03-31 11:52:14 +0700

    42.menampilkan list experience dan education (#42)
    * proteksi halaman dashboard (hanya untuk yang sudah login saja)
    
    * menampilkan data list experiece dan list education
    
    Co-authored-by: irwantosalt <irwanto.wibowo@salt.co.id>

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 
 import Spinner from "../layout/Spinner";
 import DashboardAction from "./DashboardAction";
+import Experience from "./Experience";
+import Education from "./Education";
 
 const Dashboard = ({
   getCurrentProfile,
@@ -29,6 +31,8 @@ const Dashboard = ({
       {profile !== null ? (
         <Fragment>
           <DashboardAction />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
