ce0b925fc99d7c2cdfa3fcf7c56360fdf52cec81
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-03-31 14:43:31 +0700

    43.delete experience education and account (#43)
    * proteksi halaman dashboard (hanya untuk yang sudah login saja)
    
    * hapus data experience, data education dan data account
    
    Co-authored-by: irwantosalt <irwanto.wibowo@salt.co.id>

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,7 +5,8 @@ import {
   AUTH_ERR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  ACCOUNT_DELETED
 } from "../actions/types";
 
 const initialState = {
@@ -40,6 +41,7 @@ export default function(state = initialState, action) {
     case AUTH_ERR:
     case LOGIN_FAIL:
     case LOGOUT:
+    case ACCOUNT_DELETED:
       localStorage.removeItem("token");
       return {
         ...state,
